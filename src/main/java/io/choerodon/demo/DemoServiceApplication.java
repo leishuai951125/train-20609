6ac7bccb2d160b36060a89f84f6aadce4b9b745d
leishuai <1411265409@qq.com>
2018-08-18 13:53:40 +0800

    1

--- src/main/java/io/choerodon/demo/DemoServiceApplication.java
+++ src/main/java/io/choerodon/demo/DemoServiceApplication.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //@EnableChoerodonResourceServer
 public class DemoServiceApplication {
     public static void main(String[] args){
+
         SpringApplication.run(DemoServiceApplication.class, args);
     }
 }
