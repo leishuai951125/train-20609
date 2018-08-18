a9097255257e8c0fac87a889ccd1a5b2fb412f7a
Tremble666 <1435016360@qq.com>
2018-08-18 11:23:50 +0800

    [ADD]

--- src/main/java/io/choerodon/demo/api/controller/v1/DemoController.java
+++ src/main/java/io/choerodon/demo/api/controller/v1/DemoController.java
@@ -1,6 +1,7 @@
 package io.choerodon.demo.api.controller.v1;
 
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +14,16 @@ import io.choerodon.swagger.annotation.Permission;
 @RestController
 @RequestMapping("v1/demo")
 public class DemoController {
+    @Value("${id}")
+    private String id;
+
+    @Value("${name}")
+    private String name;
 
     @GetMapping("/hello")
     @Permission(level = ResourceLevel.SITE, permissionPublic = true)
     @ApiOperation(value = "demo")
     public ResponseEntity<String> hello() {
-        return new ResponseEntity<String>("hello world", HttpStatus.OK);
+        return new ResponseEntity<String>("hello,"+id+","+name, HttpStatus.OK);
     }
 }
